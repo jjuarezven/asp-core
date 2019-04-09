a6a390db6e523f76337321293d23292e46c8b752
jose juarez <josejuarez78@gmail.com>
2019-04-09 11:00:40 -0500

    Identity, claims, policies

---
+++ IdentityDemo2/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IdentityDemo2.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace IdentityDemo2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //if (User.Identity.IsAuthenticated)
+            //{
+            //    var user = await userManager.GetUserAsync(HttpContext.User);
+            //    //await userManager.AddClaimAsync(user, new Claim("CategoriaEmpleado","4"));
+            //    var claims = User.Claims.ToList();
+            //}
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+                var user = await userManager.GetUserAsync(HttpContext.User);
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+
+            return View();
+        }
+
+        [Authorize(Policy = "PolicyCategoriaEmpleado")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IdentityDemo2/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using IdentityDemo2.Data;
+using IdentityDemo2.Models;
+using IdentityDemo2.Services;
+using IdentityDemo.Policies;
+using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityDemo2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddAuthorization(options =>
+            {
+                //options.AddPolicy("PolicyCategoriaEmpleado", pol => pol.RequireClaim("CategoriaEmpleado", new string[] { "4", "5" }));
+                options.AddPolicy("PolicyCategoriaEmpleado", pol => pol.Requirements.Add(new CategoriaEmpleadoRequirement()));
+            });
+            services.AddSingleton<IAuthorizationHandler, CategoriaEmpleadoHandler>();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
